f7551ae3a1f43ccd37b4c86803fad4a16c3cc902
Éric ALBER <eric.alber@gmail.com>
2014-09-16 23:09:29 +0200

    Add auto update feature
    Check for updates every couple of weeks

--- install/installer.sh
+++ install/installer.sh
@@ -4,3 +4,4 @@ cd $HOME
 rm -rf .git-webui 2>&1 > /dev/null
 git clone git@bitbucket.org:alberthier/git-webui.git .git-webui
 git config --global --replace-all alias.webui !$HOME/.git-webui/release/libexec/git-core/git-webui
+git config --global --replace-all webui.autoupdate true
