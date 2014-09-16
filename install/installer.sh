49e2431bdfa65069a60e0e89c61250a96f3df126
Éric ALBER <eric.alber@gmail.com>
2014-09-16 23:13:16 +0200

    Add an uninstaller Add logs to the installer

--- install/installer.sh
+++ install/installer.sh
@@ -2,6 +2,9 @@
 
 cd $HOME
 rm -rf .git-webui 2>&1 > /dev/null
+echo "Cloning git-webui repository"
 git clone git@bitbucket.org:alberthier/git-webui.git .git-webui
+echo "Installing 'webui' alias"
 git config --global --replace-all alias.webui !$HOME/.git-webui/release/libexec/git-core/git-webui
+echo "Enabling auto update"
 git config --global --replace-all webui.autoupdate true
