95538442bec1d021ee9a67a50cfefb35b1c796b4
Éric ALBER <eric.alber@gmail.com>
2014-10-15 23:32:55 +0200

    Add a commit graph
    Display links of each commit to its parents

--- src/share/git-webui/webui/js/git-webui.js
+++ src/share/git-webui/webui/js/git-webui.js
@@ -20,6 +20,24 @@ var webui = webui || {};
 
 webui.repo = "/";
 
+webui.COLORS = ["#412000", "#451904", "#5d1f0c", "#4a1700", "#490036", "#48036c", "#051e81", "#0b0779", "#1d295a", "#004b59", "#004800", "#164000", "#2c3500", "#463a09", "#401a02", "#000000",
+                "#542800", "#721e11", "#7a240d", "#721f00", "#66004b", "#5c0488", "#0626a5", "#201c8e", "#1d3876", "#005d6e", "#005400", "#1c5300", "#384400", "#4d3f09", "#581f05", "#252525",
+                "#763700", "#9f241e", "#982c0e", "#a81300", "#80035f", "#650d90", "#082fca", "#3531a3", "#1d4892", "#006f84", "#036b03", "#236600", "#445200", "#544509", "#702408", "#343434",
+                "#9a5000", "#b33a20", "#b02f0f", "#c8210a", "#950f74", "#7b23a7", "#263dd4", "#4642b4", "#1d5cac", "#00849c", "#0e760e", "#287800", "#495600", "#6c5809", "#8d3a13", "#4e4e4e",
+                "#c36806", "#c85120", "#bf3624", "#df2512", "#aa2288", "#933bbf", "#444cde", "#5753c5", "#1d71c6", "#0099bf", "#188018", "#2e8c00", "#607100", "#907609", "#ab511f", "#686868",
+                "#e47b07", "#e36920", "#d34e2a", "#ec3b24", "#ba3d99", "#9d45c9", "#4f5aec", "#615dcf", "#3286cf", "#00abca", "#279227", "#3a980c", "#6c7f00", "#ab8b0a", "#b56427", "#757575",
+                "#ff911a", "#fc8120", "#e7623e", "#fa5236", "#ca4da9", "#a74fd3", "#5a68ff", "#6d69db", "#489bd9", "#00bcde", "#36a436", "#47a519", "#798d0a", "#c1a120", "#bf7730", "#8e8e8e",
+                "#ffab1d", "#fd8c25", "#f36e4a", "#fc6148", "#d75ab6", "#b25ade", "#6575ff", "#7b77e9", "#4ea8ec", "#00d0f5", "#4eb94e", "#51af23", "#8b9f1c", "#d0b02f", "#d0853a", "#a4a4a4",
+                "#ffc51f", "#fe982c", "#fd7854", "#ff705f", "#e467c3", "#bd65e9", "#7183ff", "#8985f7", "#55b6ff", "#10dcff", "#51cd51", "#5cba2e", "#9eb22f", "#debe3d", "#e19344", "#b8b8b8",
+                "#ffd03b", "#ffae38", "#ff8a6a", "#ff7e7e", "#ef72ce", "#c56df1", "#8091ff", "#918dff", "#69caff", "#3ee1ff", "#72da72", "#71cf43", "#abbf3c", "#e6c645", "#eda04e", "#c5c5c5",
+                "#ffd84c", "#ffb946", "#ff987c", "#ff8f8f", "#fb7eda", "#ce76fa", "#90a0ff", "#9c98ff", "#74cbff", "#64e7ff", "#7ce47c", "#85e357", "#b8cc49", "#edcd4c", "#f9ad58", "#d0d0d0",
+                "#ffe651", "#ffbf51", "#ffa48b", "#ff9d9e", "#ff8de1", "#d583ff", "#97a9ff", "#a7a4ff", "#82d3ff", "#76eaff", "#85ed85", "#8deb5f", "#c2d653", "#f5d862", "#fcb75c", "#d7d7d7",
+                "#fff456", "#ffc66d", "#ffb39e", "#ffabad", "#ff9de5", "#da90ff", "#9fb2ff", "#b2afff", "#8ddaff", "#8bedff", "#99f299", "#97f569", "#cde153", "#fbe276", "#ffc160", "#e1e1e1",
+                "#fff970", "#ffd587", "#ffc2b2", "#ffb9bd", "#ffa5e7", "#de9cff", "#afbeff", "#bbb8ff", "#9fd4ff", "#9aefff", "#b3f7b3", "#a0fe72", "#dbef6c", "#fcee98", "#ffca69", "#eaeaea",
+                "#ffff90", "#ffe498", "#ffd0c3", "#ffc7ce", "#ffafea", "#e2a9ff", "#c0cbff", "#c3c1ff", "#b4e2ff", "#b1f3ff", "#c3f9c3", "#b1ff8a", "#e8fc79", "#fdf3a9", "#ffcf7e", "#f4f4f4",
+                "#ffffaa", "#ffe6ab", "#ffdad0", "#ffcade", "#ffb8ec", "#e6b6ff", "#cdd3ff", "#d3d1ff", "#c0ebff", "#c7f6ff", "#cdfccd", "#bcff9a", "#f2ffab", "#fdf3be", "#ffda96", "#ffffff"]
+
+
 webui.git = function(cmd, arg1, arg2) {
     // cmd = git command line arguments
     // other arguments = optional stdin content and a callback function:
@@ -310,6 +328,8 @@ webui.LogView = function(historyView) {
     var self = this;
 
     self.update = function(ref) {
+        $(svg).empty();
+        streams = []
         $(content).empty();
         self.nextRef = ref;
         self.populate();
@@ -321,7 +341,8 @@ webui.LogView = function(historyView) {
             // The last node is the 'Show more commits placeholder'. Remove it.
             content.removeChild(content.lastElementChild);
         }
-        webui.git("log --pretty=raw --decorate=full --max-count=" + (maxCount + 1) + " " + self.nextRef, function(data) {
+        var startAt = content.childElementCount;
+        webui.git("log --date-order --pretty=raw --decorate=full --max-count=" + (maxCount + 1) + " " + self.nextRef, function(data) {
             var start = 0;
             var count = 0;
             self.nextRef = undefined;
@@ -335,6 +356,10 @@ webui.LogView = function(historyView) {
                 var entry = new Entry(self, data.substr(start, len));
                 if (count < maxCount) {
                     content.appendChild(entry.element);
+                    if (!self.lineHeight) {
+                        self.lineHeight = Math.ceil($(entry.element).outerHeight() / 2) * 2;
+                    }
+                    entry.element.setAttribute("style", "height:" + self.lineHeight + "px");
                     if (!currentSelection) {
                         entry.select();
                     }
@@ -348,18 +373,108 @@ webui.LogView = function(historyView) {
                 start = end + 1;
                 ++count;
             }
-            canvas.height = $(content).outerHeight();
-            canvas.width = $(content).outerWidth();
-            var ctx = canvas.getContext("2d");
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            svg.setAttribute("height", $(content).outerHeight());
+            svg.setAttribute("width", $(content).outerWidth());
             if (self.nextRef != undefined) {
                 var moreTag = $('<a class="log-entry log-entry-more list-group-item">');
                 $('<a class="list-group-item-text">Show previous commits</a>').appendTo(moreTag[0]);
                 moreTag.click(self.populate);
                 moreTag.appendTo(content);
             }
+
+            self.updateGraph(startAt);
         });
     };
+
+    self.updateGraph = function(startAt) {
+        // Draw the graph
+        var currentY = (startAt + 0.5) * self.lineHeight;
+        for (var i = startAt; i < content.children.length; ++i) {
+            var entry = content.children[i].model;
+            if (!entry) {
+                break;
+            }
+            var index = 0;
+            var left = streams.length;
+
+            // Find streams to join
+            var childCount = 0;
+            var xOffset = 12;
+            var removedStreams = 0;
+            for (var j = 0; j < streams.length;) {
+                var stream = streams[j];
+                if (stream.sha1 == entry.commit) {
+                    if (childCount == 0) {
+                        // Replace the stream
+                        stream.path.setAttribute("d", stream.path.cmds + currentY);
+                        if (entry.parents.length == 0) {
+                            streams.splice(j, 1);
+                        } else {
+                            stream.sha1 = entry.parents[0];
+                        }
+                        index = j;
+                        ++j;
+                    } else {
+                        // Join the stream
+                        var x = (index + 1) * xOffset;
+                        stream.path.setAttribute("d", stream.path.cmds + (currentY - self.lineHeight / 2) + " L " + x + " " + currentY);
+                        streams.splice(j, 1);
+                        ++removedStreams;
+                    }
+                    ++childCount;
+                } else {
+                    if (removedStreams != 0) {
+                        var x = (j + 1) * xOffset;
+                        stream.path.setAttribute("d", stream.path.cmds + (currentY - self.lineHeight / 2) + " L " + x + " " + currentY);
+                    }
+                    ++j;
+                }
+            }
+
+            // Add new streams
+            for (var j = 0; j < entry.parents.length; ++j) {
+                var parent = entry.parents[j];
+                var svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                svgPath.setAttribute("style", "stroke:" + webui.COLORS[128 + index + j + 1]);
+                var x = (index + j + 1) * xOffset;
+                if (j == 0) {
+                    svgPath.cmds = "M " + x + " " + currentY + " L " + x + " ";
+                } else {
+                    var origX = (index + 1) * xOffset;
+                    svgPath.cmds = "M " + origX + " " + currentY + " L " + x + " " + (currentY + self.lineHeight / 2) + " L " + x + " ";
+                }
+                svg.appendChild(svgPath);
+                var obj = {
+                    sha1: parent,
+                    path: svgPath,
+                };
+                streams.splice(index + j, j == 0 ? 1 : 0, obj);
+            }
+            for (var j = index + j; j < streams.length; ++j) {
+                var stream = streams[j];
+                var x = (j + 1) * xOffset;
+                stream.path.cmds += (currentY - self.lineHeight / 2) + " L " + x + " " + currentY + " L " + x + " ";
+            }
+
+            var svgCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            svgCircle.setAttribute("cx", (index + 1) * xOffset);
+            svgCircle.setAttribute("cy", currentY);
+            svgCircle.setAttribute("r", 4);
+            svg.appendChild(svgCircle);
+
+            left = Math.max(left, streams.length);
+            entry.element.setAttribute("style", entry.element.getAttribute("style") + ";padding-left:" + (left + 1) * xOffset + "px");
+
+            // Debug log
+            //console.log(entry.commit, entry.parents, $.extend(true, [], streams));
+
+            currentY += self.lineHeight;
+        }
+        for (var i = 0; i < streams.length; ++i) {
+            var stream = streams[i];
+            stream.path.setAttribute("d", stream.path.cmds + currentY);
+        }
+    }
 
     function Person(data) {
         var nameEnd = data.indexOf("<");
@@ -469,10 +584,12 @@ webui.LogView = function(historyView) {
     };
 
     self.historyView = historyView;
-    self.element = $('<div id="log-view" class="list-group"><canvas></canvas><div></div></div>')[0];
-    var canvas = $("canvas", self.element)[0];
-    var content = canvas.nextElementSibling;
+    self.element = $('<div id="log-view" class="list-group"><svg xmlns="http://www.w3.org/2000/svg"></svg><div></div></div>')[0];
+    var svg = self.element.children[0];
+    var content = self.element.children[1];
     var currentSelection = null;
+    var lineHeight = null;
+    var streams = [];
 };
 
 /*
